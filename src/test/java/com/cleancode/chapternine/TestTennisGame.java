8b86b04923ade2f806411870c755b6f6162803a8
Ian Chiu <ian.chiu@tenmax.io>
2017-07-27 20:31:21 +0800

    Add necessary test dependency & Sample test class

---
+++ src/test/java/com/cleancode/chapternine/TestTennisGame.java
@@ -0,0 +1,21 @@
+package com.cleancode.chapternine;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestTennisGame {
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testAbc() {
+        assertThat(true).isTrue();
+    }
+}
