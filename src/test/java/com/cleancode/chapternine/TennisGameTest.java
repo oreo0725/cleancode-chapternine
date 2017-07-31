5ad707e97714416ee6108d05d3b6e0ee2b830798
Zen <zen@tenmax.io>
2017-08-01 00:41:34 +0800

    3 fail test

--- src/test/java/com/cleancode/chapternine/TennisGameTest.java
+++ src/test/java/com/cleancode/chapternine/TennisGameTest.java
@@ -10,11 +10,12 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class TennisGameTest {
 
+    private final String player1Name = "SuperMan";
+    private final String player2Name = "BatMan";
+    private final TennisGame game = new TennisGame(player1Name, player2Name);
+
     @Test
     public void test_getScore_WHEN_game_just_starts_THEN_player1Is0_and_player2Is0() throws Exception {
-        // GIVEN
-        TennisGame game = new TennisGame("SuperMan", "BatMan");
-
         // WHEN
         String result = game.getScore();
 
@@ -26,14 +27,22 @@ public class TennisGameTest {
     @Test
     public void test_getScore_WHEN_p1Score1_p2Score0_THEN_game_result_p1_advantage() throws Exception {
         // GIVEN
-        TennisGame game = new TennisGame("SuperMan", "BatMan");
-        game.score("SuperMan");
+        game.score(player1Name);
         // WHEN
         String result = game.getScore();
-
         // THEN
-        assertThat(result).isEqualTo("SuperMan advantage");
+        assertThat(result).isEqualTo(player1Name + " advantage");
     }
 
+    @Test
+    public void test_getScore_WHEN_p1Score0_p2Score1_THEN_game_result_p2_advantage() throws Exception {
+        // GIVEN
+        game.score(player2Name);
+        // WHEN
+        String result = game.getScore();
+        // THEN
+        assertThat(result).isEqualTo(player2Name + " advantage");
+
+    }
 
 }
