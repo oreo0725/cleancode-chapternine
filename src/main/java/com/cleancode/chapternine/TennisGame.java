d111300115275a74432bcf0c89514a2615d5b83a
Zen <zen@tenmax.io>
2017-08-01 00:42:12 +0800

    3 tests pass

--- src/main/java/com/cleancode/chapternine/TennisGame.java
+++ src/main/java/com/cleancode/chapternine/TennisGame.java
@@ -6,20 +6,29 @@ package com.cleancode.chapternine;
  */
 public class TennisGame {
     private Player player1;
+    private Player player2;
 
     public TennisGame(String player1Name,
                       String player2Name) {
         player1 = new Player(player1Name);
+        player2 = new Player(player2Name);
+
     }
 
     public String getScore() {
-        if(player1.getScore() > 0) {
+        if (player1.getScore() > 0) {
             return player1.getPlayerName() + " advantage";
+        }
+        else if(player2.getScore() > 0) {
+            return player2.getPlayerName() + " advantage";
         }
         return "LOVE all";
     }
 
     public void score(String playerName) {
-        player1.score();
+        if (playerName.equals(player1.getPlayerName()))
+            player1.score();
+        else
+            player2.score();
     }
 }
